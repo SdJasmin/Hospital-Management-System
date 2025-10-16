f7cab783c1d5a03f0366cc7f7705243e26161788
SdJasmin <163100285+SdJasmin@users.noreply.github.com>
2025-10-16 12:12:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({
+  origin: ["https://hospital-management-system-pi-topaz.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
 
 // Authentication routes
 app.use("/auth", authRoutes);
