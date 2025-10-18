ff934a766db2235ec29432fb4fd6376c586e682a
SdJasmin <163100285+SdJasmin@users.noreply.github.com>
2025-10-18 16:05:46 +0530

    Update Emailcong.js

--- backend/middlewares/Emailcong.js
+++ backend/middlewares/Emailcong.js
@@ -1,13 +1,13 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for port 465, false for other ports
-    auth: {
-      user: "sdjasmin786@gmail.com",
-      pass: "ycap enkq bmor mvef",
-    },
-  });
-
-  
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
