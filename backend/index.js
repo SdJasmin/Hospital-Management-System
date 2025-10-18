c1410438dff6513e1dafd8e35a73c7e04fc02c92
SdJasmin <163100285+SdJasmin@users.noreply.github.com>
2025-10-18 16:10:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,17 +15,24 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json());
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://hospital-management-system-pi-topaz.vercel.app"
-];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://hospital-management-system-pi-topaz.vercel.app",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
 
 
 
