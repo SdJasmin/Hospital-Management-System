5a7e0d138d252ab49610b4137fa524b653d48ae3
SdJasmin <163100285+SdJasmin@users.noreply.github.com>
2025-10-24 17:56:26 +0530

    Update Emailcong.js

--- backend/middlewares/Emailcong.js
+++ backend/middlewares/Emailcong.js
@@ -1,9 +1,9 @@
-import nodemailer from "nodemailer";
+eimport nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
